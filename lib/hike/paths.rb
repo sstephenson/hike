c9e0f8f57df43a7283396925e4413dde09e4d4bd
Suresh Shanmugam <sureshkumar.ps@gmail.com>
2010-12-11 22:27:39 +0530

    hike through the jungles of windows-land too

--- lib/hike/paths.rb
+++ lib/hike/paths.rb
@@ -1,12 +1,13 @@
+require 'pathname'
 module Hike
   class Paths < NormalizedArray
     def initialize(root = ".")
       @root = root
       super()
     end
-
+    
     def normalize_element(path)
-      path = File.join(@root, path) unless path[/^\//]
+        path = File.join(@root, path) unless Pathname.new(path.split(File::SEPARATOR)[0]+File::SEPARATOR).root?
       File.expand_path(path)
     end
   end
