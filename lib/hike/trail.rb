cbcf95a82c75de9869c57b05dbc8c69f9b89ee5b
Joshua Peek <josh@joshpeek.com>
2011-03-21 22:22:01 -0500

    Pass cache object by parameter
    Cleaner than a thread local

--- lib/hike/trail.rb
+++ lib/hike/trail.rb
@@ -19,14 +19,14 @@ module Hike
         options = extract_options!(logical_paths)
         base_path = Pathname.new(options[:base_path] || @root)
 
-        with_index_cache do
-          logical_paths.each do |logical_path|
-            logical_path = Pathname.new(logical_path)
-            if relative?(logical_path)
-              find_in_base_path(logical_path, base_path, &block)
-            else
-              find_in_paths(logical_path, &block)
-            end
+        cache = { :index => DirectoryIndex.new, :patterns => {} }
+
+        logical_paths.each do |logical_path|
+          logical_path = Pathname.new(logical_path)
+          if relative?(logical_path)
+            find_in_base_path(cache, logical_path, base_path, &block)
+          else
+            find_in_paths(cache, logical_path, &block)
           end
         end
 
@@ -39,29 +39,6 @@ module Hike
     end
 
     protected
-      def with_index_cache
-        self.cache = { :index => DirectoryIndex.new, :patterns => {} }
-        yield
-      ensure
-        self.cache = nil
-      end
-
-      def cache
-        Thread.current[:__hike_cache__]
-      end
-
-      def cache=(value)
-        Thread.current[:__hike_cache__] = value
-      end
-
-      def index
-        cache[:index]
-      end
-
-      def patterns
-        cache[:patterns]
-      end
-
       def extract_options!(arguments)
         arguments.last.is_a?(Hash) ? arguments.pop : {}
       end
@@ -74,21 +51,21 @@ module Hike
         paths.map { |path| Pathname.new(path) }
       end
 
-      def find_in_paths(logical_path, &block)
+      def find_in_paths(cache, logical_path, &block)
         dirname, basename = logical_path.split
         pathnames.each do |base_path|
-          match(base_path.join(dirname), basename, &block)
+          match(cache, base_path.join(dirname), basename, &block)
         end
       end
 
-      def find_in_base_path(logical_path, base_path, &block)
+      def find_in_base_path(cache, logical_path, base_path, &block)
         candidate = base_path.join(logical_path).expand_path
         dirname, basename = candidate.split
-        match(dirname, basename, &block) if paths_contain?(dirname)
+        match(cache, dirname, basename, &block) if paths_contain?(dirname)
       end
 
-      def match(dirname, basename)
-        matches = index.files(dirname).grep(pattern_for(basename))
+      def match(cache, dirname, basename)
+        matches = cache[:index].files(dirname).grep(pattern_for(cache, basename))
         matches = matches.map { |f| Pathname.new(f) }
         sort_matches(matches, basename).each do |path|
           yield dirname.join(path).expand_path.to_s
@@ -99,8 +76,8 @@ module Hike
         paths.any? { |path| dirname.to_s[0, path.to_s.length] == path }
       end
 
-      def pattern_for(basename)
-        patterns[basename] ||= begin
+      def pattern_for(cache, basename)
+        cache[:patterns][basename] ||= begin
           extension_pattern = extensions.map { |e| Regexp.escape(e) }.join("|")
           /^#{Regexp.escape(basename.to_s)}(?:#{extension_pattern}|)+$/
         end
